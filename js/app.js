7be02af54cd85f280b248d255854e7521ff6e363
preveira <patrick.k.reveira@gmail.com>
2024-02-01 22:44:48 -0800

    fixing branch issues

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
 'use strict';
-
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
 function displayArrayAsTable(data) {
-
 }
-
 function City(cityName, minCustomers, maxCustomers, avgCookiesPerCustomer) {
   this.cityName = cityName;
   this.minCustomers = minCustomers;
@@ -18,11 +14,9 @@ function City(cityName, minCustomers, maxCustomers, avgCookiesPerCustomer) {
   this.row = document.createElement('tr');
   this.calculateDailyTotal(); 
 }
-
 City.prototype.calculateDailyTotal = function () {
   this.dailyTotal = this.hourlySales.reduce((acc, val) => acc + val, 0);
 };
-
 City.prototype.simulateHourlySales = function () {
   let randomCustomers = getRandomInt(this.minCustomers, this.maxCustomers);
   let cookiesPurchased = Math.floor(randomCustomers * this.avgCookiesPerCustomer);
@@ -30,7 +24,6 @@ City.prototype.simulateHourlySales = function () {
   this.calculateDailyTotal(); 
   return cookiesPurchased;
 };
-
 City.prototype.drawRow = function () {
   createCell(this.cityName, this.row);
   for (let i = 0; i < this.hourlySales.length; i++) {
@@ -39,26 +32,21 @@ City.prototype.drawRow = function () {
   createCell(this.dailyTotal, this.row);
 };
 
-
 let seattle = new City("Seattle", 23, 65, 6.3);
 let tokyo = new City("Tokyo", 3, 24, 1.2);
 let dubai = new City("Dubai", 11, 38, 3.7);
 let paris = new City("Paris", 20, 38, 2.3);
 let lima = new City("Lima", 2, 16, 4.6);
 
-
 let cities = [seattle, tokyo, dubai, paris, lima];
-
 
 for (let hour = 6; hour <= 20; hour++) {
   cities.forEach(function (city) {
     city.simulateHourlySales();
   });
 }
-
 let tHeadElement = document.getElementById('table-data');
 let tBodyElement = document.getElementById('daily-totals');
-
 
 let headerRow = document.createElement('tr');
 createCell('', headerRow); 
@@ -68,12 +56,10 @@ for (let hour = 6; hour <= 20; hour++) {
 createCell('Daily Totals', headerRow); 
 tHeadElement.appendChild(headerRow);
 
-
 cities.forEach(function (city) {
   city.drawRow();
   tBodyElement.appendChild(city.row);
 });
-
 
 let totalColumn = document.createElement('tr');
 createCell('Location Totals', totalColumn); 
@@ -94,22 +80,22 @@ document.getElementById('table-form').addEventListener('submit', handleForm);
 
 function handleForm(event) {
   event.preventDefault();
+  
 
-  let name = event.target.location.value;
   let minCustomers = parseInt(event.target['min-customers'].value);
   let maxCustomers = parseInt(event.target['max-customers'].value);
   let avgCookiesPerCustomer = parseFloat(event.target['avg-cookies'].value);
-
-
+  
+  
   let newCity = new City(name, minCustomers, maxCustomers, avgCookiesPerCustomer);
-
+  
   for (let hour = 6; hour <= 20; hour++) {
     newCity.simulateHourlySales();
   }
-
+  
 
   newCity.drawRow();
 
 
   document.getElementById('table-data').appendChild(newCity.row);
-
+}
